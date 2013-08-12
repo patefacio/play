56033ab2d0b0f36ef6c852174f9eaa04b7328b46
Daniel Davidson <dbdavidson@yahoo.com>
2013-08-12 14:45:20 -0500

    added stuff example

---
+++ stuff/lib/components/asset.dart
@@ -0,0 +1,22 @@
+import "dart:html";
+import "package:polymer/polymer.dart";
+import "package:logging/logging.dart";
+
+
+final _logger = new Logger("chattel");
+
+@CustomTag("plus-asset")
+class Asset extends PolymerElement { 
+
+  // custom <class Asset>
+
+
+
+  // end <class Asset>
+}
+
+
+
+// custom <chattel>
+// end <chattel>
+
---
+++ stuff/lib/components/dossier.dart
@@ -0,0 +1,20 @@
+import "dart:html";
+import "package:polymer/polymer.dart";
+import "package:logging/logging.dart";
+
+
+final _logger = new Logger("chattel");
+
+@CustomTag("plus-dossier")
+class Dossier extends PolymerElement { 
+
+  // custom <class Dossier>
+
+  // end <class Dossier>
+}
+
+
+
+// custom <chattel>
+// end <chattel>
+
